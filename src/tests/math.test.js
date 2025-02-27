a9c885739b2cc019897c43b4146e3eaf36f44eb9
shivani <jaiswalshivani.vhits@gmail.com>
2025-02-27 20:05:22 +0530

    Add changes in deploy and math

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test('subtracts 5 - 2 to equal 3', () => {
-  expect(subtract(5, 2)).toBe(3);
+test('subtracts 6 - 2 to equal 4', () => {
+  expect(subtract(6, 2)).toBe(4);
 });
